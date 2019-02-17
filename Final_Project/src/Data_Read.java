f16cc8de14dcce18acf1584bafe9510f302f961b
bachan143 <bachanghatuary@gmail.com>
2019-02-17 22:08:56 +0530

    first commit

--- Final_Project/src/Data_Read.java
+++ Final_Project/src/Data_Read.java
@@ -13,12 +13,27 @@ public class Data_Read
 		System.out.println("input the eventid");
 		Scanner sc=new Scanner(System.in);
 		int event_id=sc.nextInt();
-		String csvFilename = "f://window/rrr.csv";
+		String csvFilename = "f://window/window1.csv";
 		CSVReader csvReader = new CSVReader(new FileReader(csvFilename));
 		String[] row = null;
 		boolean flag=false;
+		int i=0;
 		while((row = csvReader.readNext()) != null) 
 		{
+			if(i==0)
+			{
+				System.out.println("========================");
+				System.out.println(row[0]
+				          +  row[1]
+				          +" " + row[2]
+				        	+"  "+row[3]
+				        	+"   "+row[4]
+				        			);
+				i++;
+				
+			}
+			else
+			{	
 			int index=Integer.parseInt(row[3]);
 			
 			if(index==event_id)
@@ -31,6 +46,7 @@ public class Data_Read
 			        	+"\nEvent_Id:"+index
 			        	+" \nTask_Category:"+row[4]
 			        			);
+			}
 			}
 			
 			
